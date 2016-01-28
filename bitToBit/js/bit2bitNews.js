48a61ce16714f18957b53fa4b8266831ed23e087
Yarkaic <yarkaic@gmail.com>
2016-01-28 22:58:31 +0300

    changes again many times already

--- bitToBit/js/bit2bitNews.js
+++ bitToBit/js/bit2bitNews.js
@@ -7,4 +7,18 @@ $(function () {
     $('.ratingArticle').rating({
         image: 'img/ratingStar.png'
     });
+
+    $(".lines_for_mobile").click(function () {
+        $(".mainMenu li").toggle(600);
+    });
+
+    $(window).resize(function () {
+        var doc_weight = $(window).width();
+
+        if (doc_weight > 1050) {
+            $(".mainMenu li").css("display", "inline-block");
+        } else {
+            $(".mainMenu li").css("display", "none");
+        }
+    })
 });
